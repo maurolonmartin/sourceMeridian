fc2987a6bf65fbba808ecaab6f8c9d8cca7ec7de
Mauricio Londono <mauricio.londono@dhigroupinc.com>
2023-09-19 12:22:25 -0500

    firebase implementation nad delete console logs

--- src/app/main/dog-list/dog-card/dog-card.component.ts
+++ src/app/main/dog-list/dog-card/dog-card.component.ts
@@ -1,18 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dog-card',
   templateUrl: './dog-card.component.html',
   styleUrls: ['./dog-card.component.scss']
 })
-export class DogCardComponent implements OnInit {
+export class DogCardComponent {
 
   @Input({ required: true }) url!: string;
   @Input({ required: true }) breed!: string;
-
-  ngOnInit() {
-    console.log('url en el card', this.url!)
-    console.log(this.breed!)
-  }
 
 }
